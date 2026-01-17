02017836959ccb769c4752669c0ae23aa36e2dec
Xires <xires.dev@gmail.com>
2026-01-17 19:40:24 +0100

    Initial Commit

---
+++ Trackory/TrackoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackoryApp.swift
+//  Trackory
+//
+//  Created by Robin Beckmann on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct TrackoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
